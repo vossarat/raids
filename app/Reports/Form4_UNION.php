daae3a970cac9a00aaf29d8f2d830416c5e0a02a
Your Name <you@example.com>
2017-08-17 16:52:58 +0600

    2017.08.17 parent to code

--- app/Reports/Form4_UNION.php
+++ app/Reports/Form4_UNION.php
@@ -10,41 +10,28 @@ class Form4 extends Model{
 	//Форма 4 по полу
 	public static function getForm4ByGender($startdate, $enddate, $region)
 	{
-		$parent = DB::table('code')
-		->leftJoin('code as parent', 'parent.id', '=', 'code.parent_id')		
-		->select('code.id', 'code.name as codename', 'parent.id as parentid', 'parent.name as parentname')
-		->leftJoin('code as parent_parent', 'parent.parent_id', '=', 'parent_parent.id')
-		->select('code.id','parent.id as parentid', 'parent_parent.id as parent_parent_id')	
-		->get();		
-		
-		dd($parent);
-		
-		$newparent = DB::table('code')
-		->leftJoin($parent, $parent, '=', 'code.parent_id')
-		->get();
-		dd($newparent);
-		
-		
 		$dataGroupParent = DB::table('register')
 		->leftJoin('code', 'register.code_id', '=', 'code.id')
-		->leftJoin('code as parent', 'parent.id', '=', 'code.parent_id')
-		->select('register.code_id as codeid',
-			'code.parent_id as pid',
-			'parent.parent_id as ppid',
-			DB::raw('COUNT(*) as total')
+		->select('code.parent_id as codeid',
+			DB::raw('SUM(CASE WHEN (register.sex_id = 2) THEN 1 ELSE 0 END) as mens'),
+			DB::raw('SUM(CASE WHEN (register.sex_id = 3) THEN 1 ELSE 0 END) as womens'),
+			DB::raw('SUM(CASE WHEN (register.sex_id = 1) THEN 1 ELSE 0 END) as notspecified'),
+			DB::raw('COUNT(register.sex_id) as total')
 		)
 		->whereBetween('register.grantdate', [$startdate, $enddate])
-		//->where('register.code_id', '=', 39)
-		->groupBy('codeid')
-		->get();
-		
-		dd($dataGroupParent);
+		->where(
+			function($query) use ($region){
+				if($region){
+					$query->where('register.region_id', '=', $region);
+				}
+			})
+		->groupBy('code.parent_id');
 		
 
 		$viewdata = DB::table('register')
 		->leftJoin('code', 'register.code_id', '=', 'code.id')
 		->select('code.id as codeid',
-			DB::raw('SUM(CASE WHEN (register.sex_id = 2) AND code.id IN(1, 2, 3) THEN 1 ELSE 0 END) as mens'),
+			DB::raw('SUM(CASE WHEN (register.sex_id = 2) THEN 1 ELSE 0 END) as mens'),
 			DB::raw('SUM(CASE WHEN (register.sex_id = 3) THEN 1 ELSE 0 END) as womens'),
 			DB::raw('SUM(CASE WHEN (register.sex_id = 1) THEN 1 ELSE 0 END) as notspecified'),
 			DB::raw('COUNT(register.sex_id) as total')
