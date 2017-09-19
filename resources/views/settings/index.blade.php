7c27f67d62bbbb74d698289cd40758ea13414895
Your Name <you@example.com>
2017-09-19 21:08:44 +0600

    2017-09/19

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -51,13 +51,13 @@
 		<div class="form-group">
 			
 			<label for="closedate" class="control-label">Закрыть период</label>
-			<input id="closedate" type="text" class="form-control" name="closedate" value="{{ $viewdata['closedate'] or old('closedate') }}" {{ Auth::user()->id == 1 ? '' : 'disabled' }}>
+			<input id="closedate" type="text" class="form-control" name="closedate" value="{{ $viewdata['closedate'] or old('closedate') }}" {{ Auth::user()->id == 3 ? '' : 'disabled' }}>
 		</div>
 		{{-- end closedate field --}}
 		
 		{{-- button save close period --}}
 		<div class="form-group">
-			<button type="submit" class="btn btn-info" {{ Auth::user()->id == 1 ? '' : 'disabled' }}>
+			<button type="submit" class="btn btn-info" {{ Auth::user()->id == 3 ? '' : 'disabled' }}>
 				Закрыть период
 			</button>
 		</div>
